cdd0560f8701c0398af0d23eccf3e18997eed472
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-08-18 10:04:17 +0530

    Create BubbleSort.java

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countSwaps function below.
+    static void countSwaps(int[] a) {
+        int totalSwaps = 0;
+        int n = a.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1;j++){
+                if(i != j){
+                    if(a[j] > a[j+1]){
+                        int temp = a[j];
+                        a[j] = a[j+1];
+                        a[j+1] = temp;
+                        totalSwaps++;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Array is sorted in " + totalSwaps + " swaps.");
+        System.out.println("First Element: " +  a[0]);
+        System.out.println("Last Element: " + a[n-1]);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        countSwaps(a);
+
+        scanner.close();
+    }
+}
