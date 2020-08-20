49790709e3f4240adb42d2a0d0d844ebdd55f1f5
Abhishek Yelisetti <42091343+abhiy77@users.noreply.github.com>
2020-08-20 21:43:41 +0530

    Create FibonacciNumbers.cpp

---
+++ Recursion And Backtracking/FibonacciNumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n) { 
+    return (n > 2 ? (fibonacci(n-1) + fibonacci(n-2)) : 1) ;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
